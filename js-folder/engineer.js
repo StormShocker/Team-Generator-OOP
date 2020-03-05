94caba69959c7bb5655856036624b04dce089bc4
Sean Gonzalez <sean.oryn@gmail.com>
2020-03-04 21:03:22 -0600

    Make engineer class and pass tests

--- js-folder/engineer.js
+++ js-folder/engineer.js
@@ -2,9 +2,17 @@ const Employee = require('./employee');
 
 class Engineer extends Employee {
     constructor(name, id, email, github) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
+        super(name, id, email);
         this.github = github;
     }
+
+    getRole() {
+        return 'Engineer';
+    }
+
+    getGithub() {
+        return this.github;
+    }
 }
+
+module.exports = Engineer;
